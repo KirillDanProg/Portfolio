8ded20a514ea47b455f0f8c74dc2f98be5b2c33d
KirillVN <103306964+KirillVN@users.noreply.github.com>
2022-05-24 05:00:52 +0300

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector(".style-btn");
 const head = document.querySelector("head");
 
-btn.addEventListener("click", styleIt);
+btn.addEventListener("touchend", styleIt);
 
 function styleIt() {
     head.innerHTML += `<link rel="stylesheet" href="css/style.min.css" />`

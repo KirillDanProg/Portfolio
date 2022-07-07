6ff997f16c9f6c1038456b99cd750e3a48cfea47
KirillDanProg <kirill2898v@gmail.com>
2022-07-07 16:10:19 +0300

    button eventListener click added

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,6 +2,7 @@ const btn = document.querySelector(".style-btn");
 const head = document.querySelector("head");
 
 btn.addEventListener("touchend", styleIt);
+btn.addEventListener("click", styleIt);
 
 function styleIt() {
     head.innerHTML += `<link rel="stylesheet" href="css/style.min.css" />`
